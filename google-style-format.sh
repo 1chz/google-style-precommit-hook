7e4b2af469b3b87af4d9e5ee27825463a65837d0
Changhun Han <shirohoo4102@gmail.com>
2022-08-28 06:03:05 +0900

    Update google-style-format.sh

--- google-style-format.sh
+++ google-style-format.sh
@@ -8,12 +8,15 @@ cd .cache
 if [ ! -f $JAR_FILE ]
 then
     curl -LJO "https://github.com/google/google-java-format/releases/download/v$VERSION/$JAR_FILE"
-    chmod 755 $JAR_FILE
+    chmod +x $JAR_FILE
 fi
 cd ..
 
+echo "Find changed java files..."
 CHANGED_FILES=$(git diff --cached --name-only --diff-filter=ACMR | grep ".*java$" )
-echo CHANGED FILES = $CHANGED_FILES
+echo $CHANGED_FILES
+
+echo "\nApply google-java-format..."
 java \
   --add-exports jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED \
   --add-exports jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED \
@@ -21,3 +24,5 @@ java \
   --add-exports jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED \
   --add-exports jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED \
   -jar .cache/$JAR_FILE --replace --aosp $CHANGED_FILES
+
+echo "Done."
